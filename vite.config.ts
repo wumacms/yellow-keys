a85f56151f16ba3fe4c916c7fa05fe9f372d6f52
wumacms <wumacms@qq.com>
2026-03-07 09:23:19 +0800

    新增GitHub部署脚本

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/yellow-keys/',
     plugins: [vue(), tailwindcss()],
     server: {
         port: 3000,
